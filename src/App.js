9bab309cda8eeef97cfcfdcfebf64e95342002eb
tama <tama1234>
2022-08-22 12:49:35 +0700

    react socket.io

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { io } from "socket.io-client";
+import { useState } from 'react';
+
+const socket = io("ws://localhost:3000");
 
 function App() {
+  const [Progress, setProgress] = useState({ sheet: null, current: 0 });
+
+  const processData = async () => {
+    socket.removeAllListeners();
+    socket.on("eventdata", (args) => {
+      console.log('eventdata => ', args)
+      setProgress(args);
+    });
+    await fetch('http://localhost:3000/').then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div style={{ padding: 10 }}>
+        {Progress.sheet !== null && <div style={{ paddingBottom: 10 }}>
+          Please wait in upload and processing data.
+          <div>
+            <strong>Current: Sheet {Progress.sheet} and at Row {Progress.current}</strong>
+          </div>
+        </div>}
+        <button
+          type="button"
+          onClick={processData}
+          disabled={Progress.sheet !== null && Progress.sheet === 'completed'}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 disabled"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Test Process State
+        </button>
+      </div>
+    </div >
   );
 }
 
